9f53c7c4f748f0f92e81cf600f9b35a2e133731c
Chauncey Manson <Mansonchauncey@gmail.com>
2015-11-10 21:50:07 -0500

    scribble

---
+++ scribble/app/controllers/comment_controller.rb
@@ -0,0 +1,30 @@
+class CommentController < ApplicationController
+
+  def index
+    end
+
+    # new
+    def new
+    end
+
+    # create
+    def create
+    end
+
+    #show
+    def show
+    end
+
+    # edit
+    def edit
+    end
+
+    # update
+    def update
+    end
+
+    # destroy
+    def destroy
+    end
+
+  
---
+++ scribble/app/controllers/post_controller.rb
@@ -0,0 +1,28 @@
+class PostController < ApplicationController
+
+  def index
+    end
+
+    # new
+    def new
+    end
+
+    # create
+    def create
+    end
+
+    #show
+    def show
+    end
+
+    # edit
+    def edit
+    end
+
+    # update
+    def update
+    end
+
+    # destroy
+    def destroy
+    end
